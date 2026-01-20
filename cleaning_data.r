0cc1239e129c947ad5f18f6e2d5fbdbb5344171c
timtoe49-oss <timtoe49@gmail.com>
2026-01-20 16:49:16 +0000

    save file

--- cleaning_data.r
+++ cleaning_data.r
@@ -8,7 +8,19 @@ head(heating)
 
 # Tidy the data ####
 heating <- heating %>%
-  gather(key="age", value = "homes", -Source)
+  gather(key ="age", value = "homes", -Source)
 heating_tibble <- as_tibble(heating)
 heating_tibble
 
+heating_fct <- heating_tibble %>%
+  mutate(age = factor(age),
+         Source = factor(Source),
+         homes = as.integer(homes))
+heating_fct
+levels(heating_fct$age)
+levels(heating_fct$age) <- c("25-29", "30-34", "35-44", "45-54",
+                              "55-64", "65-74", "75+", "25-")
+levels(heating_fct$age)
+
+heating_tibble %>% 
+  filter(is.na(as.numeric(homes)))
